af730286828fca3af8df1c60e84a8cc5a2874b1f
GlotovAlex <alexxglotov@yandex.ru>
2025-06-05 19:52:20 +0300

    update reserves dag

--- dags/get_rf_international_reserves.py
+++ dags/get_rf_international_reserves.py
@@ -4,6 +4,7 @@ import pandas as pd
 import json
 import xml.etree.ElementTree as ET
 import datetime as dt
+from dateutil.relativedelta import relativedelta
 
 from airflow.decorators import dag, task
 from airflow.hooks.postgres_hook import PostgresHook
@@ -23,12 +24,28 @@ logger = logging.getLogger(__name__)
         'retry_delay': dt.timedelta(seconds=10),
     },
     tags=['reserves'],
+    max_active_runs=1,
 )
 
 def inter_reserves():
 
     @task()
-    def fetch_bauction_data(from_date: dt.datetime, to_date: dt.datetime):
+    def fetch_inter_reserves_data(execution_date: str):
+
+        from_date = dt.datetime.fromisoformat(execution_date) + relativedelta(months=-1)
+        to_date = dt.datetime.fromisoformat(execution_date) + relativedelta(days=-1)
+
+        logger.info(f'Получение данных за даты с {from_date} по {to_date}')
+
+        # Рабочий URL сервиса
+        url = "http://www.cbr.ru/DailyInfoWebServ/DailyInfo.asmx"
+
+        # Заголовки
+        headers = {
+            "Content-Type": "text/xml; charset=utf-8",
+            "SOAPAction": '"http://web.cbr.ru/mrrf7D"',
+            "User-Agent": "Mozilla/5.0"  # Имитация браузера
+            }
         
         body = f"""<?xml version="1.0" encoding="utf-8"?>
 <soap:Envelope xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xmlns:xsd="http://www.w3.org/2001/XMLSchema" xmlns:soap="http://schemas.xmlsoap.org/soap/envelope/">
@@ -40,7 +57,12 @@ def inter_reserves():
   </soap:Body>
 </soap:Envelope>"""
 
+        logger.info(f'Тело запроса {body}')
+
         response = requests.post(url, data=body, headers=headers)
+        response.raise_for_status()
+
+        result_xml = response.content
         
         # if response.status_code == 200:
         #     return response.content
@@ -64,7 +86,14 @@ def inter_reserves():
         # Создаем DataFrame
         df = pd.DataFrame(rows)
 
+        logging.info(f'Датафрейм создан {df.to_dict()}')
+
+
+
         # При желании можно преобразовать колонку "Дата" в datetime
         df['Дата'] = pd.to_datetime(df['Дата'])
+    
+    fetched_data = fetch_inter_reserves_data(execution_date="{{ execution_date }}")
 
-        return df
+# Регистрация DAG
+dag = inter_reserves()
